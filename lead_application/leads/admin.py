00ae773b87a044eb7888e343c1e8e9492407738d
Chris Loew <cloew123@gmail.com>
2013-09-18 16:34:42 -0600

    Updated leads model to have property information

--- lead_application/leads/admin.py
+++ lead_application/leads/admin.py
@@ -6,7 +6,12 @@ class PointOfContactInline(admin.TabularInline):
     extra = 1
 
 class LeadAdmin(admin.ModelAdmin):
-    list_display = ('owner_name',)
-    #inlines = [OwnerInline]
+    list_display = ('property_street_address', 'owner_name')
+    fieldsets = [
+        (None,               {'fields': ['first_name', 'last_name']}),
+        ('Property information', {'fields': ['folio_id', 'property_street_address', 'property_city', 'property_state', 'property_zip_code'], 'classes': ['collapse']}),
+    ]
+    
+    inlines = [PointOfContactInline]
 
 admin.site.register(Lead, LeadAdmin)
--- lead_application/leads/models.py
+++ lead_application/leads/models.py
@@ -3,10 +3,16 @@ from django.utils import timezone
 from django.db import models
 
 class Lead(models.Model):
-    investor_name = models.CharField(max_length=200)
+    # Owner Information
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
     
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    # Property Information
+    folio_id = models.CharField(max_length=200, blank=True, null=True)
+    property_street_address = models.CharField(max_length=200, blank=True, null=True)
+    property_city = models.CharField(max_length=200, blank=True, null=True)
+    property_state = models.CharField(max_length=200, blank=True, null=True)
+    property_zip_code = models.CharField(max_length=200, blank=True, null=True)
     
     def owner_name(self):
         return self.first_name + " " + self.last_name

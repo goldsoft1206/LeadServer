9291a5176b2e712e6d5a8d1151cf4278bb1255d6
Chris Loew <cloew123@gmail.com>
2013-09-19 17:32:14 -0600

    Tidied up some of the formatting of the leads page

--- lead_application/leads/admin.py
+++ lead_application/leads/admin.py
@@ -1,16 +1,22 @@
 from leads.models import Investor, Lead, PointOfContact
 from django.contrib import admin
 
-class PointOfContactInline(admin.TabularInline):
+class PointOfContactInline(admin.StackedInline):
     model = PointOfContact
     extra = 1
+    
+    fieldsets = [
+        (None,                  {'fields': [('first_name', 'last_name', 'email')]}),
+        ('Address',             {'fields': ['street_address', ('city', 'state', 'zip_code')], 'classes': ['collapse']}),
+        ('Telephones', {'fields': [('telephone1', 'telephone2', 'telephone3')], 'classes': ['collapse']}),
+    ]
 
 class LeadAdmin(admin.ModelAdmin):
     list_display = ('property_street_address', 'owner_name', 'telephone1')
     fieldsets = [
         (None,                  {'fields': ['investor']}),    
-        ('Owner',               {'fields': ['first_name', 'last_name', 'owner_street_address', 'owner_city', 'owner_state', 'owner_zip_code', 'telephone1', 'telephone2', 'telephone3', 'email'], 'classes': ['collapse']}),
-        ('Property information', {'fields': ['folio_id', 'property_street_address', 'property_city', 'property_state', 'property_zip_code', 'property_bedroom_number', 'property_bathroom_number', 'property_inside_sq_ft', 'property_lot_size', 'property_year_built', 'auction_pending'], 'classes': ['collapse']}),
+        ('Owner',               {'fields': [('first_name', 'last_name'), 'owner_street_address', ('owner_city', 'owner_state', 'owner_zip_code'), ('telephone1', 'telephone2', 'telephone3'), 'email'], 'classes': ['collapse']}),
+        ('Property information', {'fields': ['folio_id', 'property_street_address', ('property_city', 'property_state', 'property_zip_code'), 'property_bedroom_number', 'property_bathroom_number', 'property_inside_sq_ft', 'property_lot_size', 'property_year_built', 'auction_pending'], 'classes': ['collapse']}),
         ('Short Sale Lender information', {'fields': ['short_sale_lender_name', 'short_sale_lender_telephone', 'short_sale_lender_letter_fax', 'point_of_contact', 'lender_verify_info', 'loan_number'], 'classes': ['collapse']}),
     ]
     

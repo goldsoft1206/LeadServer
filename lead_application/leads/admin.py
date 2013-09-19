0b950e8e29be89113a0881992a25e652c323bcb0
Chris Loew <cloew123@gmail.com>
2013-09-19 14:36:59 -0600

    Added most of the data to the leads

--- lead_application/leads/admin.py
+++ lead_application/leads/admin.py
@@ -8,8 +8,10 @@ class PointOfContactInline(admin.TabularInline):
 class LeadAdmin(admin.ModelAdmin):
     list_display = ('property_street_address', 'owner_name', 'telephone1')
     fieldsets = [
-        (None,               {'fields': ['first_name', 'last_name', 'telephone1']}),
-        ('Property information', {'fields': ['folio_id', 'property_street_address', 'property_city', 'property_state', 'property_zip_code'], 'classes': ['collapse']}),
+        #(None,                  {'fields': ['first_name', 'last_name', 'telephone1']}),    
+        ('Owner',               {'fields': ['first_name', 'last_name', 'owner_street_address', 'owner_city', 'owner_state', 'owner_zip_code', 'telephone1', 'telephone2', 'telephone3', 'email'], 'classes': ['collapse']}),
+        ('Property information', {'fields': ['folio_id', 'property_street_address', 'property_city', 'property_state', 'property_zip_code', 'property_bedroom_number', 'property_bathroom_number', 'property_inside_sq_ft', 'property_lot_size', 'property_year_built', 'auction_pending'], 'classes': ['collapse']}),
+        ('Short Sale Lender information', {'fields': ['short_sale_lender_name', 'short_sale_lender_telephone', 'short_sale_lender_letter_fax', 'point_of_contact', 'lender_verify_info', 'loan_number'], 'classes': ['collapse']}),
     ]
     
     inlines = [PointOfContactInline]
--- lead_application/leads/models.py
+++ lead_application/leads/models.py
@@ -3,10 +3,19 @@ from django.utils import timezone
 from django.db import models
 
 class Lead(models.Model):
+    # status 
     # Owner Information
+    deceased = models.BooleanField()
     first_name = models.CharField(max_length=200, blank=True, null=True)
     last_name = models.CharField(max_length=200, blank=True, null=True)
+    owner_street_address = models.CharField(max_length=200, blank=True, null=True)
+    owner_city = models.CharField(max_length=200, blank=True, null=True)
+    owner_state = models.CharField(max_length=200, blank=True, null=True)
+    owner_zip_code = models.CharField(max_length=200, blank=True, null=True)
     telephone1 = models.CharField(max_length=200, blank=True, null=True)
+    telephone2 = models.CharField(max_length=200, blank=True, null=True)
+    telephone3 = models.CharField(max_length=200, blank=True, null=True)
+    email = models.CharField(max_length=200, blank=True, null=True)
     
     # Property Information
     folio_id = models.CharField(max_length=200, blank=True, null=True)
@@ -14,6 +23,22 @@ class Lead(models.Model):
     property_city = models.CharField(max_length=200, blank=True, null=True)
     property_state = models.CharField(max_length=200, blank=True, null=True)
     property_zip_code = models.CharField(max_length=200, blank=True, null=True)
+    # Property Characteristics
+    property_bedroom_number = models.IntegerField(default=0, blank=True, null=True)
+    property_bathroom_number = models.IntegerField(default=0, blank=True, null=True)
+    property_inside_sq_ft = models.IntegerField(default=0, blank=True, null=True)
+    property_lot_size = models.IntegerField(default=0, blank=True, null=True)
+    property_year_built = models.IntegerField(default=0, blank=True, null=True)
+    # Tax Info
+    auction_pending = models.BooleanField()
+    
+    # Short Sale Lender
+    short_sale_lender_name = models.CharField(max_length=200, blank=True, null=True)
+    short_sale_lender_telephone = models.CharField(max_length=200, blank=True, null=True)
+    short_sale_lender_letter_fax = models.CharField(max_length=200, blank=True, null=True)
+    point_of_contact = models.CharField(max_length=200, blank=True, null=True)
+    lender_verify_info = models.CharField(max_length=200, blank=True, null=True)
+    loan_number = models.CharField(max_length=200, blank=True, null=True)
     
     def owner_name(self):
         return self.first_name + " " + self.last_name
@@ -25,6 +50,14 @@ class PointOfContact(models.Model):
     lead = models.ForeignKey(Lead)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    street_address = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    state = models.CharField(max_length=200, blank=True, null=True)
+    zip_code = models.CharField(max_length=200, blank=True, null=True)
+    telephone1 = models.CharField(max_length=200, blank=True, null=True)
+    telephone2 = models.CharField(max_length=200, blank=True, null=True)
+    telephone3 = models.CharField(max_length=200, blank=True, null=True)
+    email = models.CharField(max_length=200, blank=True, null=True)
     
     def __unicode__(self):
         return self.first_name + " " + self.last_name
